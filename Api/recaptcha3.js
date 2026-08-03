da4b885d30605040df98d502e4a2aa5eb035738c
vwxyzarii17 <markocop170104@gmail.com>
2026-08-03 18:24:08 +0700

    Update recaptcha3.js

--- Api/recaptcha3.js
+++ Api/recaptcha3.js
@@ -1,4 +1,4 @@
-async function recaptchaV3({ domain, siteKey, action }, page) {
+async function recaptchaV3({ domain, siteKey, action = "login" }, page) {
 
     if (!domain)
         throw new Error("Missing domain parameter");
@@ -6,66 +6,46 @@ async function recaptchaV3({ domain, siteKey, action }, page) {
     if (!siteKey)
         throw new Error("Missing siteKey parameter");
 
-    const timeout = 120000;
+    const timeout = global.timeOut || 120000;
 
-    return new Promise(async (resolve, reject) => {
-
-        let finished = false;
-
-        const timer = setTimeout(() => {
-
-            if (!finished) {
-                finished = true;
-                reject(new Error("Timeout Error"));
-            }
-
-        }, timeout);
-
-        try {
-
-            await page.goto(domain, {
-                waitUntil: "domcontentloaded"
-            });
-
-            await page.addScriptTag({
-                url: `https://www.google.com/recaptcha/api.js?render=${siteKey}`
-            });
-
-            const token = await page.evaluate(async (siteKey, action) => {
-
-                await new Promise(resolve => grecaptcha.ready(resolve));
-
-                return grecaptcha.execute(siteKey, {
-                    action: action || "login"
-                });
-
-            }, siteKey, action);
-
-            clearTimeout(timer);
-
-            if (!token || token.length < 10)
-                throw new Error("Failed to get token");
-
-            finished = true;
-
-            resolve({
-                success: true,
-                type: "recaptcha3",
-                token
-            });
-
-        } catch (err) {
-
-            clearTimeout(timer);
-
-            if (!finished) {
-                finished = true;
-                reject(err);
-            }
-
-        }
-
+    await page.goto(domain, {
+        waitUntil: "domcontentloaded"
     });
+
+    await page.addScriptTag({
+        url: `https://www.google.com/recaptcha/api.js?render=${siteKey}`
+    });
+
+    const token = await Promise.race([
+
+        page.evaluate(async (siteKey, action) => {
+
+            await new Promise(resolve => grecaptcha.ready(resolve));
+
+            return grecaptcha.execute(siteKey, {
+                action
+            });
+
+        }, siteKey, action),
+
+        new Promise((_, reject) => {
+
+            setTimeout(() => {
+                reject(new Error("Timeout Error"));
+            }, timeout);
+
+        })
+
+    ]);
+
+    if (!token || typeof token !== "string" || token.length < 10)
+        throw new Error("Failed to get token");
+
+    return {
+        success: true,
+        type: "recaptcha3",
+        token
+    };
 
 }
 
